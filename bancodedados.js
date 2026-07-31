7881cb35569b8deef7bacaf46fac50ce251d0745
lorena-gpcode <lorenaggama@gmail.com>
2026-07-31 16:59:35 -0300

    Configura conexao com o banco de dados MongoDB

---
+++ bancodedados.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const dns = require('dns')
+
+// Garante a resolução DNS no Node.js
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+async function conectabancodedados() {
+    try {        
+        console.log("A conexao com o banco de dados iniciou")
+
+        const uri = process.env.MONGO_URL
+
+        await mongoose.connect(uri, {
+            serverSelectionTimeoutMS:5000
+        })
+    
+        console.log('A conexao com o banco de dados foi estabelecida com sucesso!')
+    } catch (error) {
+        console.error('Erro ao conectar como banco de dados:', error.message)
+        
+    }    
+}
+conectabancodedados()
